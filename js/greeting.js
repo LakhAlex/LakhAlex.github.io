4a5ecd235d355b95b83cf7b2d41547c11fa8b3f1
Lakh <135232784+LakhAlex@users.noreply.github.com>
2025-02-14 15:25:48 +0900

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -2,13 +2,15 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+const toDoLists = document.querySelector("#divFormAndUl");
+
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "userName";
 
 function onLoginSubmit(tomate) {
-    tomate.preventDefault();
+    // tomate.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const userNameThattheUserWrote = loginInput.value;
     localStorage.setItem(USERNAME_KEY, userNameThattheUserWrote);
@@ -31,5 +33,6 @@ if (savedUsername === null){  // localStorage에 아무 것도 없을 경우
     // submit 이벤트가 발생하면 -> inLoginSubmit이라는 함수를 실행함
 }else{
     // show the greetings
+    toDoLists.classList.remove(HIDDEN_CLASSNAME);
     printGreetings(savedUsername);
 }
